d55340fa52308e5b9f2c66615b4d9c9ee1edc5f5
Go Eun-Seok <gomonk3037@gmail.com>
2017-10-30 20:28:19 +0900

    Create relative-time component for utc time

---
+++ content/src/components/Common/RelativeTime.vue
@@ -0,0 +1,21 @@
+<template>
+  <time class="relativeTime" :datetime="utc">{{timeago}}</time>
+</template>
+
+<script>
+import timeago from 'timeago.js';
+
+const timeagoInstance = timeago();
+
+export default {
+  name: 'RelativeTime',
+  props: {
+    utc: String,
+  },
+  computed: {
+    timeago() {
+      return timeagoInstance.format(this.utc, 'en');
+    },
+  },
+};
+</script>
