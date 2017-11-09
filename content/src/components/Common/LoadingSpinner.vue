65f5dcc50c013d27103139d4c58305a3d583e0cb
Go Eun-Seok <gomonk3037@gmail.com>
2017-11-09 16:10:22 +0900

    Add loading-spinner component for api request

---
+++ content/src/components/Common/LoadingSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="loadingSpinner">
+    <grid-loader />
+  </div>
+</template>
+
+<script>
+import GridLoader from 'vue-spinner/src/GridLoader';
+
+export default {
+  name: 'LoadingSpinner',
+  components: {
+    GridLoader,
+  },
+};
+</script>
