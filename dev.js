7825d4a5a55f76065200da05e789246921258363
Go Eun-Seok <gomonk3037@gmail.com>
2017-11-09 16:13:15 +0900

    Force link open in parent window not inner iframe

--- dev.js
+++ dev.js
@@ -1,11 +1,18 @@
 const iframe = document.createElement('iframe');
 iframe.id = 'vGitssue';
 iframe.onload = function() {
+  const frameWin = iframe.contentWindow;
+
+  // force link from iframe to be opened in the parent window
+  const base = document.createElement('base');
+  base.target = '_blank';
+  frameWin.document.head.appendChild(base);
+  // hot reload for developing
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://localhost:3000/app.js', true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-      iframe.contentWindow.eval(xhr.responseText);
+      frameWin.eval(xhr.responseText);
     }
   }
   xhr.send();
