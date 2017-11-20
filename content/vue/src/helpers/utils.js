96360baa0c552aa531b173bf48343913cccf91b6
Go Eun-Seok <gomonk3037@gmail.com>
2017-11-20 19:55:25 +0900

    Add method 'request' to utils for github graphql api request

--- content/vue/src/helpers/utils.js
+++ content/vue/src/helpers/utils.js
@@ -23,6 +23,19 @@ export default {
       .catch(res => reject(res.error));
     });
   },
+  request({ token, query }) {
+    return new Promise((resolve) => {
+      axios.post('https://api.github.com/graphql', {
+        query,
+      }, {
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+      }).then((res) => {
+        resolve(res.data.data);
+      });
+    });
+  },
   message(port, msg) {
     window.parent.postMessage({ port, msg }, '*');
   },
