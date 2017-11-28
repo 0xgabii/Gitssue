a40e20cf313676e24da506e9af7ba2d9289fee81
Go Eun-Seok <gomonk3037@gmail.com>
2017-11-28 17:17:10 +0900

    Add modal component & layout styling

---
+++ content/vue/src/components/Common/Modals/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="modal" ref="modal">
+    <div class="modal__backdrop" @click="$emit('close')" />
+    <div class="modal-wrapper">
+      <div class="modal-header">
+        <h3 class="modal-header__title">{{title}}</h3>        
+      </div>
+      <div class="modal-body">
+        <slot />
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    title: String,
+  },
+  mounted() {
+    document.body.appendChild(this.$refs.modal);
+  },
+};
+</script>
