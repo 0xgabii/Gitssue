e276c0050bdcf28ecf6bb420c3044f71fedd02c7
Go Eun-Seok <gomonk3037@gmail.com>
2017-12-05 02:56:13 +0900

    Add global vue component modal

--- content/vue/src/helpers/global.js
+++ content/vue/src/helpers/global.js
@@ -2,6 +2,7 @@ import Vuebar from 'vuebar';
 import VueScrollTo from 'vue-scrollto';
 import ToggleButton from 'vue-js-toggle-button';
 
+import Modal from '../components/Common/Modals/Layout';
 import InfiniteScroll from '../components/Common/InfiniteScroll';
 import LoadingSpinner from '../components/Common/LoadingSpinner';
 import RelativeTime from '../components/Common/RelativeTime';
@@ -11,6 +12,7 @@ const install = (Vue) => {
   Vue.use(ToggleButton);
   Vue.use(Vuebar);
 
+  Vue.component('modal', Modal);
   Vue.component('infinite-scroll', InfiniteScroll);
   Vue.component('loading-spinner', LoadingSpinner);
   Vue.component('relative-time', RelativeTime);
