fe15697e0f707dedfc907101582cd3c252eed62a
Go Eun-Seok <gomonk3037@gmail.com>
2017-12-25 12:40:27 +0900

    Fixed iframe is not mounted when page load is complete

--- content/inject/index.js
+++ content/inject/index.js
@@ -1,9 +1,23 @@
-const iframe = document.createElement('iframe');
-iframe.id = 'vGitssue';
-iframe.src = 'http://localhost:3000';
-iframe.onload = function() {
+function startInjection() {
+  const iframe = document.createElement('iframe');
+  iframe.id = 'vGitssue';
+  iframe.src = 'http://localhost:3000';
+
+  document.body.appendChild(iframe);
+}
+
+function startReceiveMessage() {
   window.addEventListener('message', ({ data }) => {
-    message.process[data.port](data.msg);
+    if (message.process[data.port].constructor === Function) {
+      message.process[data.port](data.msg);
+    };
   });
 }
-document.body.appendChild(iframe);
+
+const interval = setInterval(() => {
+  if(Object.keys(message.process).length) {
+    startInjection();
+    startReceiveMessage();
+    clearInterval(interval);
+  }
+}, 500);
