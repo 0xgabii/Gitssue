57b3a66134275dea1b97f73a31bc80531a3d40b0
Go Eun-Seok <gomonk3037@gmail.com>
2018-01-07 16:52:03 +0900

    Change profile page for user's issue management

--- src/components/Pages/Profile.vue
+++ src/components/Pages/Profile.vue
@@ -1,16 +1,75 @@
 <template>
-  <div class="profile">
-      <img 
-        class="profile__avatar"
-        :src="user.avatarUrl"
-      />
-      <h1 class="profile__name">{{user.name}}</h1>
-      <h3 class="profile__nickName">{{user.login}}</h3>
-      <button 
-        class="profile__signOut"
-        @click="signOut">
-        Sign out
-      </button>
+  <div class="profile board">
+
+    <div class="board__title">
+      Recent issues you've created
+    </div>
+
+    <div class="board-content">
+      
+      <div 
+        class="repo"
+        v-for="repo in repoWithIssues"
+        :key="repo.name">
+
+        <div class="repo-info">
+          <h3>{{repo.owner}}/{{repo.name}}</h3>
+          <router-link 
+            class="ion-plus-round"
+            v-tooltip.top="'Create new issue'"
+            tag="i"            
+            replace
+            :to="{ name: 'NewIssue', params: { owner: repo.owner, name: repo.name } }"
+          />
+        </div>
+
+        <div class="repo-issues">
+
+          <router-link
+            v-for="issue in repo.issues"
+            tag="div"
+            class="issue"
+            replace
+            :class="`issue--${issue.state.toLowerCase()}`"
+            :key="issue.id"
+            :to="{
+              name: 'Issue',
+              params: { 
+                owner: issue.repository.owner,
+                name: issue.repository.name ,
+                number: issue.number 
+              }
+            }">
+            
+            <span
+              v-for="label in issue.labels"
+              class="issue__tag"
+              :key="label.id"
+              :style="{ color: label.color, backgroundColor: label.bgColor }">
+              {{label.name}}
+            </span>
+
+            <h3 class="issue__title">
+              {{issue.title}}
+            </h3>
+
+            <p class="issue-info">
+              <span>
+                <relative-time :utc="issue.time" />
+              </span>
+              <span v-if="issue.comments">
+                <i class="ion-ios-chatboxes" />{{issue.comments}}
+              </span>
+            </p>
+            
+          </router-link>
+
+        </div>
+
+      </div>
+      
+    </div>
+
   </div>
 </template>
 
@@ -27,17 +86,46 @@ export default {
       login: '',
       avatarUrl: '',
     },
+
+    issues: [],
   }),
   computed: {
     ...mapState('resource', [
       'auth',
     ]),
+    repoWithIssues() {
+      const repos = [];
+
+      this.issues.forEach(({ repository }) => {
+        // eslint-disable-next-line        
+        if (!repos.find(({ owner, name }) => {
+          return repository.owner === owner && repository.name === name;
+        })) {
+          repos.push(repository);
+        }
+      });
+
+      this.issues.forEach((item) => {
+        // eslint-disable-next-line
+        const findIndex = repos.findIndex(({ owner, name }) => {
+          return item.repository.owner === owner && item.repository.name === name;
+        });
+
+        if (findIndex > -1) {
+          repos[findIndex] = {
+            ...repos[findIndex],
+            issues: [
+              ...repos[findIndex].issues || [],
+              item,
+            ],
+          };
+        }
+      });
+
+      return repos;
+    },
   },
   methods: {
-    signOut() {
-      utils.message('auth', { type: 'signOut' });
-    },
-
     request() {
       utils.request({
         token: this.auth,
@@ -46,10 +134,77 @@ export default {
             name
             login
             avatarUrl
+
+            issues(first: 20 orderBy:{field:CREATED_AT direction:DESC}) {
+              edges {
+                node {
+                  id
+                  number
+                  title
+                  author {
+                    login
+                  }
+                  labels (first: 3) {
+                    nodes {
+                      id
+                      name
+                      color
+                    }
+                  }
+                  state
+                  closed
+                  createdAt
+                  closedAt
+                  comments {
+                    totalCount
+                  }
+                  repository{
+                    name
+                    owner {
+                      login
+                    }
+                  }
+                }
+              }
+            }
           }
         }`,
       }).then(({ viewer }) => {
-        this.user = viewer;
+        this.user = {
+          ...viewer,
+        };
+
+        this.issues = viewer.issues.edges.map(({ node: {
+          id,
+          number,
+          title,
+          author,
+          labels,
+          state,
+          closed,
+          closedAt,
+          createdAt,
+          comments,
+          repository,
+        } }) => ({
+          id,
+          number,
+          title,
+          author: author.login,
+          labels: labels ? labels.nodes.map(node => ({
+            id: node.id,
+            name: node.name,
+            color: parseInt('ffffff', 16) / 2 > parseInt(node.color, 16) ? '#fff' : '#000',
+            bgColor: `#${node.color}`,
+          })) : [],
+          state,
+          time: closed ? closedAt : createdAt,
+          comments: comments.totalCount,
+          repository: {
+            owner: repository.owner.login,
+            name: repository.name,
+          },
+        }));
       });
     },
   },
